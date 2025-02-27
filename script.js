cd99f5805656caadf561f6fc3b37ecb2b8ea8550
Manu BuddHa <ferano915@gmail.com>
2025-02-27 21:02:45 +0530

    Grid Creation Working

---
+++ script.js
@@ -0,0 +1,27 @@
+const container=document.querySelector("#container");
+let size=document.getElementById("size");
+grid=size.value;
+const color=document.getElementById("color");
+function create_grid(size){
+   container.innerHTML="";
+   for(let i=0;i<size;i++) //create no. of rows
+   {
+      const div=document.createElement("div");
+      div.classList.add("col");
+      container.appendChild(div);
+      console.log(i);
+   }
+   const row=document.querySelectorAll(".col");
+   row.forEach(box=>{
+      for(i=0;i<size;i++){
+         const newDiv=document.createElement("div");
+         newDiv.classList.add("grid");
+         box.appendChild(newDiv);
+      }
+   })
+
+}
+function generate(){
+   console.log("Clicked");
+   create_grid();
+}
